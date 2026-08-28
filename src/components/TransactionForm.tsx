8e245999d86ad07eb24f42e5ceb20d8331d06532
beauticodez <angellorl_94@hotmail.com>
2026-08-28 13:25:55 -0500

    feat: make category required in transaction form

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -241,9 +241,10 @@ export default function TransactionForm({ onTransactionAdded }: Props) {
           <select
             value={categoryId}
             onChange={(e) => setCategoryId(e.target.value)}
+            required
             className="w-full border border-gray-300 rounded-lg px-3 py-2 text-gray-800 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           >
-            <option value="">Sin categoria</option>
+            <option value="">Seleccionar categoria</option>
             {filteredCategories.map((c) => (
               <option key={c.id} value={c.id}>
                 {c.name}
